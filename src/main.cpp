4e6429e041504e8f8d913da371c17bbf21f7d7ad
izitktj <universoreversoizi@gmail.com>
2024-09-02 08:51:47 -0300

    adicionadas coisas interessantes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <windows.h>
 #include <SDL.h>
+#include <iostream>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Inicializa a SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         return 1;
     }
 
     SDL_Window* window = SDL_CreateWindow(
-        "Minha Primeira Janela SDL",
+        "Window",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         800, 600,
@@ -20,7 +20,38 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
-    SDL_Delay(5000);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!renderer) {
+        std::cout << "Renderer error" << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+    bool running = true;
+
+    while(running)
+    {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                std::cout << "Exiting" << std::endl;
+                running = false;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        
+        SDL_RenderDrawPoint(renderer, 100, 100);
+
+        SDL_RenderPresent(renderer);
+    }
+
     SDL_DestroyWindow(window);
     SDL_Quit();
 
